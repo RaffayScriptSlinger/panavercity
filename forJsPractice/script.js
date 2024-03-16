ffa6005ef5578eb383111c895665243c89e6127a
RaffayScriptSlinger <raffaysharjeel@gmail.com>
2024-03-15 23:13:10 -0930

    Some Changes

---
+++ forJsPractice/script.js
@@ -0,0 +1,178 @@
+// var course = prompt("Enter Your course");
+// if(course === "wma"){
+//     console.log("You are endrolled in wma");
+// }else{
+//     console.log("please input write course");
+// }
+
+// var age = +prompt("enter  Your Age");
+// if(age <= 18){
+//     console.log("You are eligible");
+// }
+// else  {
+//     console.log("You are not Eligible");
+// }
+
+// if(course === "wma" && age  <= 18 ){
+//     document.write("congrats You Are Eligible" + "<br>");
+// }else {
+//     document.write("sorry try again " + "<br>")
+// }
+
+
+// // Ecercise Number two
+
+// var mathsNumber = +prompt("Enter your Maths Number");
+// if (mathsNumber == "") {
+//     alert("Enter Please");
+//     mathsNumber = +prompt("Enter your Maths Number");
+//     if(mathsNumber == ""){
+//         alert("ENter Please")
+//     }
+// }
+
+
+
+// var urduNumber = +prompt("Enter your Urdu Number");
+
+
+// var computerNumber = +prompt("Enter your Computer Number");
+
+
+// var engNumber = +prompt("Enter your English Number");
+
+
+// var islamiatNumber = +prompt("Enter your Islamiat Number");
+
+
+// var physicsNumber = +prompt("Enter your Physics Number");
+
+
+
+// var totalNumber = (mathsNumber + urduNumber + computerNumber + engNumber + islamiatNumber + physicsNumber);
+
+// var entireNumber = 550 ;
+
+// alert("tatal Number is = "  + totalNumber);
+
+// alert("Your Percentage is " +  totalNumber / entireNumber * 100 );
+
+
+// document.write("Your Maths Number is"  + mathsNumber + "<br>" ) ;
+// document.write("your Urdu Number is" + urduNumber + "<br>");
+// document.write("your Islamiat Number is" + islamiatNumber + "<br>");
+// document.write("your computer Number is" + computerNumber + "<br>");
+// document.write("your English Number is" + engNumber + "<br>");
+// document.write("your Physics Number is" + physicsNumber + "<br>");
+
+
+// // Class Practice
+
+// let age = 17;
+// let citylocation ="karachi";
+// let education ="intermediate"
+
+
+// if(age >= 17){
+//     if(citylocation = "karachi"){
+//         if (education = "intermediate"){
+//             console.log("You are Eligible")
+//         }
+//         else {
+//             console,log("Tru Again")
+//         }
+//     }
+//         else{
+//             console.log("must br lived in karachi")
+//         }
+//     }
+//         else{
+//             console.log("Minimm age 18 Required")
+//         };
+    
+    
+// let Number = +prompt ("Enter A Number ");
+
+
+//  if(Number % 3 == 0 && Number % 5 == 0){
+//     console.log("Its Mutiple of Both 5 & 3")
+// }
+// else if( Number % 3 == 0){
+//     console.log("It is Multiple Of Three")
+// }
+// else if( Number % 5 == 0){
+//     console.log("It is Multiple of Five")
+// }
+
+// else{
+//     console.log("Its Not Multipe of five and Three")
+// }
+
+// Next Practice
+
+// let age = +prompt("Enter Your Age");
+
+// if(age <= 18 ){
+//     document.write("Your Age Is Less Than 18")
+// }
+// else if(age >= 18 && age <= 60){
+//     document.write("Your Age is Perfect")
+// }
+// else if(age > 60){
+//     document.write("you are To Old")
+// }
+// else{
+//     document.write("Please Input Correct Age")
+// }
+
+// Next Practice
+
+// let Cities = [ 
+//     "karachi", "Multan" , "Quetta" , "Islamabad"
+// ]
+
+// Cities.push("Peshawar");
+// console.log(Cities)
+    
+ // Next Practice
+
+ let name =[ ]
+ name.push("Sharjeel") 
+ name.push("Rehan")
+ name.push("Raffay")
+ name.push("Shayan")
+ name.push("Shaheer")
+ document.write("After Push Method " + "<br>")
+ document.write(name + "<br>"  + "<br>");
+
+ // Push se ham array me last me ek or data store karate han
+
+ document.write("After Pop Statement" + "<br>")
+ name.pop()
+document.write(name);
+ // Pop se ham array me last me ek  data Remove karate han
+
+
+// Shift And Unshift Practice
+
+let Result = [ "98" , "77" , "44" , "56" , "72" ];
+Result.shift();
+
+Result.shift();
+
+// Shift ke zarya ham start se ek data ko remove karte han
+
+Result.unshift = (123)
+Result.unshift = (88)
+
+// Unshift ke cammand ke zarya ham array ke under last me data store karte han
+
+var fullName = []
+fullName.push("Raffay")
+fullName.push("Sharjeel")
+
+fullName.pop()
+
+fullName.shift()
+
+
